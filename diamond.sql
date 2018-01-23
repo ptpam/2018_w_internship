3bbeafff4123cc088f25ec689871b15531a3e9f4
ermismel <ermismelike@gmail.com>
2018-01-23 10:39:23 +0300

    code is revised

--- diamond.sql
+++ diamond.sql
@@ -3,31 +3,23 @@ DECLARE
  vNewLine  VARCHAR2(250);
  
 BEGIN
-    cntr := 0;
+  cntr := 0;
   WHILE cntr <= 13  LOOP
     vNewLine := '';
     cntr:= cntr +1;
-    IF cntr < 7 THEN
-       FOR Lcntr IN 1.. 7-cntr LOOP
+       FOR Lcntr IN 1.. ABS(7-cntr) LOOP
             vNewLine := CONCAT(vNewLine, ' ');
        END LOOP;
-       FOR Lcntr IN 1.. 2*cntr-1 LOOP
-            vNewLine := CONCAT(vNewLine, '*');
-       END LOOP;
+       IF cntr <= 7 THEN 
+           FOR Lcntr IN  REVERSE 1.. 2*MOD(cntr,14)-1 LOOP
+                vNewLine := CONCAT(vNewLine, '*');
+           END LOOP;
+       ELSE 
+           FOR Lcntr IN  REVERSE 1.. 2*(14-cntr)-1  LOOP
+                vNewLine := CONCAT(vNewLine, '*');
+           END LOOP;
+       END IF;
        dbms_output.put_line(vNewLine);
-    ELSIF cntr > 7 THEN
-       FOR Lcntr IN 1.. cntr-7 LOOP
-            vNewLine := CONCAT(vNewLine, ' ');
-       END LOOP;
-       FOR Lcntr IN 1.. 2*(14-cntr)-1 LOOP
-            vNewLine := CONCAT(vNewLine, '*');
-       END LOOP;
-       dbms_output.put_line(vNewLine);
-    ELSE
-        FOR Lcntr IN 1.. 2*cntr-1 LOOP
-            vNewLine := CONCAT(vNewLine, '*');
-       END LOOP;
-       dbms_output.put_line(vNewLine);
-    END IF;
   END LOOP;
 END;  
+ 
